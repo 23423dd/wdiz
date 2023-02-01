f0829f99e16b61bbb2ac49d47758be7e0313d789
23423dd <124113423+23423dd@users.noreply.github.com>
2023-02-01 11:42:35 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-const hamburgerBtn = document.getElementById('hamburger-btn')
-const nav = document.getElementById('nav')
+$('#hamburger-btn').on('click',function() {
+    $('#nav').toggleClass('open')
+})
 
 
-hamburgerBtn.addEventListener('click', function () {
-    nav.classList.toggle('open')
+function navInit() {
+    $(window).on('scroll',function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $("#header").addClass("stickyadd");
+        } else {
+            $("#header").removeClass("stickyadd");
+        }
+    });
+} 
 
-}) 
-
+navInit();
 
 
 function navInit() {
