117e3b5e9d328c9fcbb2d3df830e89cf33e2b801
23423dd <124113423+23423dd@users.noreply.github.com>
2023-02-01 11:46:56 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $('#hamburger-btn').on('click',function() {
 })
 
 
+//jQ 스크롤 이벤트를 잘 몰라서 샘플페이지에서 베꼈습니다.
 function navInit() {
     $(window).on('scroll',function() {
         var scroll = $(window).scrollTop();
@@ -16,16 +17,3 @@ function navInit() {
 
 navInit();
 
-
-function navInit() {
-    $(window).on('scroll',function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 50) {
-            $("#header").addClass("stickyadd");
-        } else {
-            $("#header").removeClass("stickyadd");
-        }
-    });
-} 
-
-navInit();
